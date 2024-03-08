6942cca4d135f876c9cc5c76c44f585fb4bc92f2
hadil <hihijazi@gmail.com>
2024-03-08 13:02:15 -0500

    PROJECT COMPLETE

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
          <div style={{ padding: '75px' }}>
            <h2 style={{ fontSize: '36px' }}>Welcome to Bookify Books</h2>
            <p style={{ fontSize: '24px' }}>FIND YOUR PLACE AT BOOKIFY BOOKS BOOKSTORE</p>
-           <p style={{ fontSize: '20px' }}>Feel free to explore our books, review orders, and add new books to the inventory.</p>
+           <p style={{ fontSize: '20px' }}>Feel free to explore our books, view nearby bookstores, and signup so an account.</p>
          </div>
   );
  };
